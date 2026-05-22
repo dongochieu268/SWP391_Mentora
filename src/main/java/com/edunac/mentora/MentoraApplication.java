c961949b824940b98ba3ef7131cbfd873050f423
Kieu Tuan Minh <kieutuanminh1708@gmail.com>
2026-05-22 21:45:23 +0700

    codebase

---
+++ src/main/java/com/edunac/mentora/MentoraApplication.java
@@ -0,0 +1,13 @@
+package com.edunac.mentora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edunac/mentora/MentoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edunac.mentora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

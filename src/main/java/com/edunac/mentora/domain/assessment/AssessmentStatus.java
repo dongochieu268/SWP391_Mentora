d2ef2c0f684abe36f6f83b95a9563d5af25857fa
Le Tung <letunganh23042006@gmail.com>
2026-06-03 19:56:08 +0700

    feat(assessment): add assessment data model

---
+++ src/main/java/com/edunac/mentora/domain/assessment/AssessmentStatus.java
@@ -0,0 +1,7 @@
+package com.edunac.mentora.domain.assessment;
+
+public enum AssessmentStatus {
+    DRAFT,
+    PUBLISHED,
+    ARCHIVED
+}
---
+++ src/main/java/com/edunac/mentora/domain/assessment/QuestionDifficulty.java
@@ -0,0 +1,7 @@
+package com.edunac.mentora.domain.assessment;
+
+public enum QuestionDifficulty {
+    EASY,
+    MEDIUM,
+    HARD
+}

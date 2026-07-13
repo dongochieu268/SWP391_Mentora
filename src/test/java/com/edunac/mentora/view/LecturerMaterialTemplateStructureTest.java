fb61f78c53a894dfc2f05de50bba11522e287cf6
Kieu Tuan Minh <kieutuanminh1708@gmail.com>
2026-07-13 16:40:34 +0700

    Cover material question picker layout

--- src/test/java/com/edunac/mentora/view/LecturerMaterialTemplateStructureTest.java
+++ src/test/java/com/edunac/mentora/view/LecturerMaterialTemplateStructureTest.java
@@ -42,6 +42,21 @@ class LecturerMaterialTemplateStructureTest {
     }
 
     @Test
+    void questionBankPickerIsSeparatedIntoSearchableModal() throws IOException {
+        String template = resource("templates/lecturer/material/list.html");
+
+        assertTrue(template.contains("data-bs-target=\"#materialQuestionPickerModal\""));
+        assertTrue(template.contains("id=\"materialQuestionPickerModal\""));
+        assertTrue(template.contains("id=\"materialQuestionSearch\""));
+        assertTrue(template.contains("id=\"materialQuestionTypeFilter\""));
+        assertTrue(template.contains("id=\"materialQuestionDifficultyFilter\""));
+        assertTrue(template.contains("id=\"materialQuestionPreviousPage\""));
+        assertTrue(template.contains("id=\"materialQuestionNextPage\""));
+        assertTrue(template.contains("data-question-bank-row"));
+        assertTrue(template.contains("syncMaterialQuestionPicker"));
+    }
+
+    @Test
     void materialPageDoesNotContainMojibakeText() throws IOException {
         String template = resource("templates/lecturer/material/list.html");
 
